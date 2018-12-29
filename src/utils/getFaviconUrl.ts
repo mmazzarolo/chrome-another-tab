75613386cae55ef6df1324a098d4a73c0f988375
mmazzarolo <mazzarolomatteo@gmail.com>
2018-12-29 11:45:20 +0100

    Use chrome://favicon/ locally

--- src/utils/getFaviconUrl.ts
+++ src/utils/getFaviconUrl.ts
@@ -1,7 +1,11 @@
 export const getFaviconUrl = (url: string) => {
   try {
-    const prefixLessUrl = new URL(url || "").hostname;
-    return `https://api.faviconkit.com/${prefixLessUrl}/32`;
+    if (process.env.NODE_ENV === "development") {
+      const prefixLessUrl = new URL(url || "").hostname;
+      return `https://api.faviconkit.com/${prefixLessUrl}/32`;
+    } else {
+      return `chrome://favicon/size/16@2x/${url}`;
+    }
   } catch (err) {
     return "";
   }
