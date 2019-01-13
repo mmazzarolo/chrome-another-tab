a46f71b3b0f7f27ab271bfb38f710e8a58db46f9
mmazzarolo <mazzarolomatteo@gmail.com>
2019-01-13 13:41:23 +0100

    Disable redux-logger in production

--- src/utils/createReduxStore.ts
+++ src/utils/createReduxStore.ts
@@ -3,6 +3,7 @@ import { createLogger } from "redux-logger";
 import createSagaMiddleware from "redux-saga";
 import { rootReducer } from "../reducers";
 import { rootSaga } from "../sagas";
+import { NODE_ENV } from "../config/constants";
 
 const loggerMiddleware = createLogger({
   collapsed: true,
@@ -12,7 +13,11 @@ const loggerMiddleware = createLogger({
 const sagaMiddleware = createSagaMiddleware();
 
 export const createReduxStore = (initialState?: {}) => {
-  const middlewares = [loggerMiddleware, sagaMiddleware];
+  const middlewares = [];
+  if (NODE_ENV === "development") {
+    middlewares.push(loggerMiddleware);
+  }
+  middlewares.push(sagaMiddleware);
   const enhancer = compose(applyMiddleware(...middlewares));
   const store = createStore(rootReducer, initialState!, enhancer);
   sagaMiddleware.run(rootSaga);
