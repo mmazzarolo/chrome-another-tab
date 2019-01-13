36922a7ae785490cd67e87cd01a22267e7e05fae
mmazzarolo <mazzarolomatteo@gmail.com>
2019-01-13 16:32:07 +0100

    Updates

---
+++ precache-manifest.cc1d59837274e2239838825d80e48640.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "d99db59ab9606fd5db28",
+    "url": "/chrome-another-tab/static/js/main.d99db59a.chunk.js"
+  },
+  {
+    "revision": "830faad97f42cb159d5c",
+    "url": "/chrome-another-tab/static/js/1.830faad9.chunk.js"
+  },
+  {
+    "revision": "f5ed3f3d449619abfb97",
+    "url": "/chrome-another-tab/static/js/runtime~main.f5ed3f3d.js"
+  },
+  {
+    "revision": "e930db039f8a3787e3f43df3a016d412",
+    "url": "/chrome-another-tab/index.html"
+  }
+];
